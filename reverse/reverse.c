ee2f432667ac80b73e9e8e4793516e2360300162
SUSHANTHPRAJ <157506971+SUSHANTHPRAJ@users.noreply.github.com>
2024-08-11 17:02:22 +0530

    Reverse code

---
+++ reverse/reverse.c
@@ -0,0 +1,52 @@
+// 1. Write a loop for testing - sizes 1-10
+// 2. 2-digit random numbers
+// 3. Create a pull requests
+
+#include<stdio.h>
+#include<stdlib.h>
+void revarray(int* array, int size)
+{
+    int temp;
+    for (int i = 0, j = size - 1; i < j; i++, j--)
+    {
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+}
+void reverse_and_print(int arr[], int size)
+{
+    printf("Before reverse:\n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    revarray(arr, size);
+    printf("Reversed array:\n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int array[10];
+    for (int size = 3; size <= 7; size++)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = rand() % 100;
+        }
+        reverse_and_print(array, size);
+
+    }
+    return 0;
+}
+
+
+
+
