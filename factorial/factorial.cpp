13cd53f4a99f4c23abffff514ae458b336bc9f89
SUSHANTHPRAJ <157506971+SUSHANTHPRAJ@users.noreply.github.com>
2024-08-15 12:15:34 +0530

    output factorial of 0 is 1

--- factorial/factorial.cpp
+++ factorial/factorial.cpp
@@ -1,21 +1,22 @@
 #include<stdio.h>
 int factorial(int n)
 {
-	if (n == 0)
+	if (n < 0)
+	{
+		return 0;
+	}
+	if (n == 0 || n == 1)
 	{
 		return 1;
 	}
-	else
-	{
-		return n * factorial(n - 1);
-	}
+	return n * factorial(n - 1);
 }
 int main()
 {
-	int number = 27; // test solution
+	int number =0 ; // test solution
 	int result = factorial(number);
 	printf("factorial of %d is %d\n", number, result);
 	return 0;
 }
 //output
-//factorial of 27 is 1484783616
+//factorial of 0 is 1
