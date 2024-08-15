c699049e1595f9647dccf94d7e12fbcebaf2cd1c
SUSHANTHPRAJ <157506971+SUSHANTHPRAJ@users.noreply.github.com>
2024-08-15 13:00:13 +0530

    comparing recursion with loop

--- factorial/factorial.cpp
+++ factorial/factorial.cpp
@@ -1,22 +1,38 @@
 #include<stdio.h>
-int factorial(int n)
+unsigned long long factorial_loop(int n)
+{
+	unsigned long long result = 1;
+	if (n < 0)
+	{
+		return 0;
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		result *= i;
+	}
+	return result;
+}
+unsigned long long factorial_recursion(int n)
 {
 	if (n < 0)
 	{
 		return 0;
 	}
-	if (n == 0 || n == 1)
+	if (n == 0)
 	{
 		return 1;
 	}
-	return n * factorial(n - 1);
+	return n * factorial_recursion(n - 1);
 }
 int main()
 {
-	int number =-3 ; // test solution
-	int result = factorial(number);
-	printf("factorial of %d is %d\n", number, result);
+	int number = 4;
+	unsigned long long loop_result = factorial_loop(number);
+	unsigned long long recursion_result = factorial_recursion(number);
+	printf("Factorial of %d using loop: %llu\n", number, loop_result);
+	printf("Factorial of %d using recursion: %llu\n", number, recursion_result);
 	return 0;
 }
-//output
-//factorial of -3 is 0
+// output
+// Factorial of 4 using loop: 24
+// Factorial of 4 using recursion: 24
