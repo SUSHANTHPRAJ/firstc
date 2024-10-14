10e848484e57098cee675658d92e2d84f59d3e5b
SUSHANTHPRAJ <157506971+SUSHANTHPRAJ@users.noreply.github.com>
2024-10-14 10:10:20 +0530

    hexa to octa program

---
+++ hexa/hexa.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+void hexToDecimal(char hex[], long* decimal)
+{
+    *decimal = 0;
+    for (int i = 0; hex[i] != '\0'; i++)
+    {
+        *decimal *= 16;
+        if (hex[i] >= '0' && hex[i] <= '9')
+        {
+            *decimal += hex[i] - '0';
+        }
+        else if (hex[i] >= 'A' && hex[i] <= 'F')
+        {
+            *decimal += hex[i] - 'A' + 10;
+        }
+        else if (hex[i] >= 'a' && hex[i] <= 'f')
+        {
+            *decimal += hex[i] - 'a' + 10;
+        }
+    }
+}
+
+void decimalToOctal(long decimal, char octal[])
+{
+    int index = 0;
+    while (decimal != 0)
+    {
+        octal[index++] = (decimal % 8) + '0';
+        decimal /= 8;
+    }
+    octal[index] = '\0';
+    for (int i = 0; i < index / 2; i++)
+    {
+        char temp = octal[i];
+        octal[i] = octal[index - i - 1];
+        octal[index - i - 1] = temp;
+    }
+}
+
+int main()
+{
+    char hex[20];
+    long decimal;
+    char octal[20];
+    printf("Enter a hexadecimal number: ");
+    scanf_s("%s", &hex);
+    hexToDecimal(hex, &decimal);
+    decimalToOctal(decimal, octal);
+    printf("Octal equivalent: %s\n", octal);
+    return 0;
+}
